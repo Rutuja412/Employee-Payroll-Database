834e7baac10088b1ab3ceed96081d9335eaa73ea
Rutuja412 <rutujakadam4545@gmail.com>
2023-02-15 13:03:10 +0530

    [Rutuja]:create table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,11 @@
 ﻿--Welcome
+--Create UC1
+Create database EMPLOYEE_PAYROLL
+-- UC-2 Create Employee_Payroll Table
+create Table EmployeeTable
+(
+id int identity primary key,
+name varchar(200) not null,
+salary money not null,
+start_Date date not null
+)
