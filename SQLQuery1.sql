0f8551fc7cd85f36cf78de386ab8c244836629ca
Rutuja412 <rutujakadam4545@gmail.com>
2023-02-15 13:18:27 +0530

    [Rutuja]:Insert Employee Data

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,11 +1,17 @@
 ﻿--Welcome
---Create UC1
+--UC1-Create database
 Create database EMPLOYEE_PAYROLL
 -- UC-2 Create Employee_Payroll Table
 create Table EmployeeTable
 (
 id int identity primary key,
-name varchar(200) not null,
+name varchar(100) not null,
 salary money not null,
 start_Date date not null
 )
+--UC-3 Insert values in Table
+Insert into EmployeeTable values
+('Rutuja',40000,'2023-02-15'),
+('Vikram',50000,'2019-05-23'),
+('Rohit',60000,'2020-02-12'),
+('Sneha',70000,'2021-03-14')
